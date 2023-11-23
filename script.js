83726c4699d592ea6f54d07f4823868b4d751744
imran <87297004+imranshaikh9930@users.noreply.github.com>
2023-11-23 15:17:52 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,14 +1,13 @@
 function firstWord(s) {
   // your code here
 	
-	if(s.length === 0 || !s.includes(" ")){
-		return s
-	}
-	else{
-		const firstIndex = s.indexOf(" ");
-	const sliceed = s.slice(0,firstIndex);
-	return sliceed;
-	}
+	if (s.length === 0 || !s.includes(" ")) {
+    return s;
+  } else {
+    const firstSpaceIndex = s.indexOf(" ");
+    const firstWord = s.slice(0, firstSpaceIndex);
+    return firstWord;
+  }
 	
 	
 	
